af3b1cc04cf5b98d6501363ccf377c50f0557062
suraj singh rajput <surajthakurrs45@gmail.com>
2024-07-08 16:19:36 +0530

    header working

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,13 @@ import Images from "../../constant/Images";
 import { Link } from "react-router-dom";
 import { toggleDarkMode } from "../../store/slice/DarkModeStateSlice";
 import { useDispatch } from "react-redux";
+import style from "./style.module.css";
+
+import { MdClose } from "react-icons/md";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
 function Header() {
+    const[menuOpen,setMenuOpen]=useState(false)
   const dispatch = useDispatch();
   const toggleHandle = (e) => {
     // [true] dark mode enable
@@ -16,15 +22,19 @@ function Header() {
     }
   };
   return (
-    <header className="flex items-center">
+    <header className="flex items-center lg:block lg:pl-2 relative">
       <div className="logo">
         <Link to="/">
           <img src={Images.logo} alt="logo" className="w-[100px] h-[90px]" />
         </Link>
       </div>
 
-      <div className="flex list-none p-2 text-xl justify-evenly flex-1">
-        <li>
+      <div className="menuBtn">
+        <MdClose className="w-8 h-8 absolute top-5 right-2 4xl:hidden lg:block" />
+      </div>
+
+      <div className="flex list-none p-2 text-xl justify-evenly flex-1 lg:block lg:px-4">
+        <li className={style.hover}>
           <Link to="">Skills</Link>
         </li>
         <li>
@@ -46,7 +56,7 @@ function Header() {
           <Link to="">Contact Me</Link>
         </li>
       </div>
-      <div className="w-[150px]">
+      <div className="w-[150px] lg:w-[100%] lg:flex lg:justify-start">
         <div className="toggle">
           <input type="checkbox" id="darkMode" onClick={toggleHandle} />
           <label htmlFor="darkMode">
